aa505e4095927345aa9dae3b14e2b5be9f49db43
SadiaSookram <sadiamsookram@gmail.com>
2021-07-31 04:21:49 -0400

    Global Var's added to JS file

---
+++ assets/js/dashboard.js
@@ -0,0 +1,4 @@
+var searchBox = document.getElementById("search-box")
+var selectedCity = document.getElementById("City")
+var savedLocations = document.getElementById("saved-locations")
+var Forecast = document.getElementById("foreecast")
