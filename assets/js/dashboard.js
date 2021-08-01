db5a604d2f0a38821910e13a9e3f63c36512a794
SadiaSookram <sadiamsookram@gmail.com>
2021-08-01 17:25:56 -0400

    API key successfully added too dashboard

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -7,43 +7,38 @@ var Forecast = document.getElementById("foreecast");
 var searchButtonEl = document.getElementById("searchButton");
 var apiKey= "7fe1cc8350b277fdeddb1d18efc3c5c1"  
 var userInput = document.getElementById('citySearch').value;
-let object = {
-    q : window.userInput = document.getElementById('citySearch').value,
-    
-  };
-//   console.log(object);
 
-// //api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
-// //https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
+//Event Listener
+    //searchButton = document.getElementById('searchButton');
+    searchButtonEl.addEventListener('click',CitySearch)
 
+//api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+//https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
 
+ 
+function grabCity(param) { 
 // // Get weather for searched city
-// //var getApiUrl ='https://api.openweathermap.org/data/2.5/weather?q'+ citySearch.value +'&appid=7fe1cc8350b277fdeddb1d18efc3c5c1';
-// // Make request by search
-// fetch('https://api.openweathermap.org/data/2.5/weather?appid=7fe1cc8350b277fdeddb1d18efc3c5c1&',{
-//     method: 'POST',
-//     body: 'q=' +  encodeURIComponent(JSON.stringify(object)),
-// }) .then(function(response) {
-//     // response comes back correct
-//     if (response.ok) {
-//         response.json().then(function(data) {
-//             cityWeather(data,city);
-//             console.log(data);
-//             console.log(city);
-//         });
-//     } else {
-//         alert("Issue: our server is unable to process");
-//     }
-// })
-//     .catch(function(error) {
-//         console.log("Something Happened!");
-//      })
-
-  function getParam(param){
-      console.log('1st func', param)
-  }  
-
+    var getApiUrl ='https://api.openweathermap.org/data/2.5/weather?q=' + param + '&appid=7fe1cc8350b277fdeddb1d18efc3c5c1'
     
+// // Make request by search
+ fetch(getApiUrl).then(function(response) {
+     // response comes back correct
+     if (response.ok) {
+         response.json().then(function(data) {
+
+                    
+           console.log(data);           
+           console.log(param);
+        });
+     } else {
+        alert("Issue: our server is unable to process");
+    }
+ })
+    .catch(function(error) {
+    console.log("Something Happened!");
+    })}
+
+ 
 // User input to searchbar
     function CitySearch() {
         console.log('clicked')
@@ -53,11 +48,10 @@ let object = {
     
     if (citySearch === '')
     alert("You havent entered a city! Try again");
-    FuncSearch(City);
+    grabCity(city);
     };
 
-    //searchButton = document.getElementById('searchButton');
-    searchButtonEl.addEventListener('click',CitySearch)
+
 
     
 
