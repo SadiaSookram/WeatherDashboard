d8bcd35cb0c87869724df37d44b9892f31dfa44a
SadiaSookram <sadiamsookram@gmail.com>
2021-08-01 23:55:39 -0400

    Adding data to Html

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -7,6 +7,8 @@ var Forecast = document.getElementById("foreecast");
 var searchButtonEl = document.getElementById("searchButton");
 var apiKey= "7fe1cc8350b277fdeddb1d18efc3c5c1"  
 var userInput = document.getElementById('citySearch').value;
+var makeCard = document.getElementById("currentDay")
+const myStorage = window.localStorage;
 
 //Event Listener
     //searchButton = document.getElementById('searchButton');
@@ -14,7 +16,7 @@ var userInput = document.getElementById('citySearch').value;
 
 //api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
 //https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
-
+//https://api.openweathermap.org/data/2.5/weather?lat=43.700111&lon=-79.416298&appid={yourkey}
  
 function grabCity(param) { 
 // // Get weather for searched city
@@ -25,10 +27,17 @@ function grabCity(param) {
      // response comes back correct
      if (response.ok) {
          response.json().then(function(data) {
-
-                    
-           console.log(data);           
-           console.log(param);
+           console.log(data);
+           var Citydata = data['name'];;
+           var temp = data['main']['temp'];
+           var icon = data['main']['icon'];
+           var hum = data ['main']['humidity'];
+           var weatherValue = data['weather'][0]['description'];
+                  Citydata.innerhtml = cityValue;
+                  temp.innerhtml =  tempValue;
+                  weatherValue.innerhtml= SummaryValue;
+                  icon.innerhtml= iconsValue;
+                  hum.innerhtml = humidityValue;
         });
      } else {
         alert("Issue: our server is unable to process");
@@ -36,20 +45,34 @@ function grabCity(param) {
  })
     .catch(function(error) {
     console.log("Something Happened!");
-    })}
 
- 
+})};
+   
+// Display Current Weather on Screen 
+    function cityWeather(data){
+      var makeCard = document.getElementById("currentDay")
+   
+
+    } 
+
+
 // User input to searchbar
     function CitySearch() {
         console.log('clicked')
     const city= window.userInput = document.getElementById('citySearch').value;
     
-    console.log(city); 
-    
+    console.log(city);
+  // If search bar left null
     if (citySearch === '')
     alert("You havent entered a city! Try again");
     grabCity(city);
-    };
+   
+   };
+   // Saving data in local storage
+   myStorage = myStorage.seItem(savedLocations);
+
+
+
 
 
 
